091c716cf0c4d62dfb5ff56a2bac5ad1738a67b4
ayundadella <ayundadella07@gmail.com>
2020-02-01 09:42:32 +0700

    coba

---
+++ 5-2.php
@@ -0,0 +1,12 @@
+<?php
+$x=50;
+$y=50;
+
+if ($x==$y){
+	echo "BENAR";
+}
+else{
+	echo "SALAH";
+}
+
+?>
---
+++ 5-4.php
@@ -0,0 +1,16 @@
+<?php
+$x =10;
+	echo $x;
+echo "<br>";
+$x += 100;
+	echo $x;
+echo "<br>";
+$x -= 30;
+	echo $x;
+echo "<br>";
+$x *= 5;
+	echo $x;
+echo "<br>";
+$x /= 5;
+	echo $x;
+?>
